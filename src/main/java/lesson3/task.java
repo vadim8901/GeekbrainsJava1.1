9825332a566131ea7db603d42eb78f25754d8f2b
vadim8900 <vadim-gryaznykh@yandex.ru>
2020-11-28 19:03:56 +0300

    lesson3

---
+++ src/main/java/lesson3/task.java
@@ -0,0 +1,29 @@
+package lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class task{
+
+    private static Random random = new Random();
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        do {
+            int numComp = random.nextInt(10);
+            System.out.println("Угдай число с 3 попыток от 0 до 9");
+            for (int i = 3; i > 0; i--) {
+                System.out.println("Введи свой ответ: (для выхода из игры, просто нажмите Enter)");
+                int questionNum = scanner.nextInt();
+                if (questionNum == numComp) {
+                    System.out.println("Вы угадали число, молодец!");
+                    break;
+                }
+                System.out.println(questionNum > numComp ? "Введенное чилсо больше " : "Введенное число меньше ");
+                System.out.println((i-1) > 0 ? " у вас осталось " + (i-1) + " попыток." : "Попытки закончились, вы проиграли.");
+            }
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет)");
+        }
+        while (scanner.nextInt() == 1);
+    }
+}
